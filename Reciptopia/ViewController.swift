708adc52d519e2903c632c2a1933213787f3bc7b
김세영 <yy0867@gmail.com>
2022-01-13 20:31:05 +0900

    initial commit

---
+++ Reciptopia/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Reciptopia
+//
+//  Created by 김세영 on 2022/01/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
