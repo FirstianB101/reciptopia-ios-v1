0da727661cefee12ab3217a2c7222686e728ad41
김세영 <yy0867@gmail.com>
2022-02-22 03:19:04 +0900

    UIButton과 UIColor에 자주 쓰일 Extension 추가
    UIButton - font를 설정할 수 있도록 setTitleFont(_:) 메서드 추가
    UIColor - Hex RGB로 UIColor을 얻을 수 있도록 convenience init 추가 및 accentColor 추가

---
+++ Reciptopia_UIKit/Extension/UIButtonExtension.swift
@@ -0,0 +1,15 @@
+//
+//  UIButtonExtension.swift
+//  Reciptopia_UIKit
+//
+//  Created by 김세영 on 2022/02/22.
+//
+
+import UIKit
+
+extension UIButton {
+  
+  public func setTitleFont(_ font: UIFont) {
+    self.titleLabel?.font = font
+  }
+}
