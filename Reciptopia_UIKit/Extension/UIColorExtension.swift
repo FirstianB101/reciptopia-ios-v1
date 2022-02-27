81ea5c80d008d118cf7cbb29497c6aeadcbfe2dd
김세영 <yy0867@gmail.com>
2022-02-27 09:10:14 +0900

    collectionViewBackground와 mainIngredient 색상 추가

--- Reciptopia_UIKit/Extension/UIColorExtension.swift
+++ Reciptopia_UIKit/Extension/UIColorExtension.swift
@@ -19,4 +19,6 @@ extension UIColor {
   }
   
   public static let accentColor = UIColor(0x5F8FED)
+  public static let collectionBackground = UIColor(0xE5E5E5)
+  public static let mainIngredient = UIColor(0xEAAD11)
 }
