998b0e6f19daaf1e4b649f9da04cc547a278e481
김세영 <yy0867@gmail.com>
2022-03-04 17:31:59 +0900

    CoreData의 ValueTransformer 구현 및 등록

--- Reciptopia/AppDelegate.swift
+++ Reciptopia/AppDelegate.swift
@@ -17,12 +17,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   let dependencyContainer = ReciptopiaDependencyContainer()
   
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-    
+    IngredientEntityValueTransformer.registerTransformer()
     window = UIWindow(frame: UIScreen.main.bounds)
     
     let pictureIngredientViewController = dependencyContainer.makePictureIngredientViewController()
     window?.makeKeyAndVisible()
     window?.rootViewController = UINavigationController(rootViewController: pictureIngredientViewController)
+    
     
     return true
   }
