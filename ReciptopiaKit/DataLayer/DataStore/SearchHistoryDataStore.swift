ccd979e1c01b94f0ba1aa6a081086610229521b0
김세영 <yy0867@gmail.com>
2022-03-12 02:13:54 +0900

    timestamp를 업데이트하기 위해 SearchHistoryDataStore에 update 메서드 추가

--- ReciptopiaKit/DataLayer/DataStore/SearchHistoryDataStore.swift
+++ ReciptopiaKit/DataLayer/DataStore/SearchHistoryDataStore.swift
@@ -11,6 +11,7 @@ import PromiseKit
 public protocol SearchHistoryDataStore {
   func fetch(_ page: Int) -> Promise<[History]>
   func save(_ history: History) -> Promise<History>
+  func update(_ history: History) -> Promise<Void>
   func delete(_ history: History) -> Promise<Void>
   func deleteAll() -> Promise<Void>
 }
