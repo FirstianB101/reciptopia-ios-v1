33e2d15e7102f6486dfeda1e6409944fbeb4ff6d
김세영 <yy0867@gmail.com>
2022-03-12 02:33:22 +0900

    Date를 String으로 변환하는 편의 메서드 구현

---
+++ ReciptopiaKit/Infrastructure/Extension/String+Date.swift
@@ -0,0 +1,35 @@
+//
+//  String+Date.swift
+//  ReciptopiaKit
+//
+//  Created by 김세영 on 2022/03/12.
+//
+
+import Foundation
+
+public extension Date {
+  func toString(format: String) -> String {
+    let formatter = DateFormatter()
+    formatter.calendar = Calendar.current
+    formatter.timeZone = TimeZone.current
+    formatter.locale = Locale.current
+    formatter.dateFormat = format
+    return formatter.string(from: self)
+  }
+  
+  func toString(dateFormat: DateFormat) -> String {
+    switch dateFormat {
+      case .iso8601:
+        let formatter = ISO8601DateFormatter()
+        formatter.timeZone = TimeZone.current
+        return formatter.string(from: self)
+      default:
+        return toString(format: dateFormat.rawValue)
+    }
+  }
+}
+
+public enum DateFormat: String {
+  case iso8601
+  case yearMonthDate = "YYYY년 M월 d일"
+}
